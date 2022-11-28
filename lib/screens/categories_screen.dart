d922b0a52a0b966be39d2a715cd4fc8ad9892aa6
luscamamede <eumesmolucassilva@gmail.com>
2022-11-28 10:59:37 -0300

    new file:   fonts/Raleway-Black.ttf 	new file:   fonts/Raleway-Bold.ttf 	new file:   fonts/Raleway-Regular.ttf 	new file:   fonts/RobotoCondensed-Bold.ttf 	new file:   fonts/RobotoCondensed-Italic.ttf 	new file:   fonts/RobotoCondensed-Light.ttf 	new file:   fonts/RobotoCondensed-Regular.ttf 	modified:   lib/components/category_item.dart 	modified:   lib/main.dart 	new file:   lib/screens/categories_meals_screen.dart 	modified:   lib/screens/categories_screen.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../components/category_item.dart';
-import '../../../data/dummy_data.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
@@ -12,6 +12,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Vamos Cozinhar?'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
