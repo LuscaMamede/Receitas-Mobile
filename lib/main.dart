373e154fc1094c77fa12ee6cb996a74a2061e218
luscamamede <eumesmolucassilva@gmail.com>
2022-11-29 16:08:28 -0300

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'screens/tabs_screen.dart';
-// import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'utils/app_routes.dart';
