69934461b10e4b1ab716d7f3ff9a23ba1314d970
luscamamede <eumesmolucassilva@gmail.com>
2022-11-29 08:26:57 -0300

    modified:   lib/components/category_item.dart 	modified:   lib/data/dummy_data.dart 	modified:   lib/main.dart 	new file:   lib/models/meal.dart 	modified:   lib/screens/categories_meals_screen.dart 	new file:   lib/utils/app_routes.dart

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  const CategoriesMealsScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings.arguments as Category;
