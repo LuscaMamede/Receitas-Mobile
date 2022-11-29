b9352d921285439efeebb1121c17c294085e20a4
luscamamede <eumesmolucassilva@gmail.com>
2022-11-29 15:36:54 -0300

    new file:   lib/components/main_drawer.dart 	modified:   lib/screens/tabs_screen.dart

---
+++ lib/components/main_drawer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({super.key});
+
+  Widget _createItem(IconData icon, String label) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        label,
+        style: const TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 24,
+            fontWeight: FontWeight.bold),
+      ),
+      onTap: (() {}),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: 110,
+            padding: EdgeInsets.all(10),
+            color: Theme.of(context).colorScheme.primary,
+            alignment: Alignment.center,
+            child: const Text(
+              'Vamos Cozinhar!',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 30,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          _createItem(Icons.restaurant, 'Refeições'),
+          _createItem(Icons.settings, 'Configurações'),
+        ],
+      ),
+    );
+  }
+}
