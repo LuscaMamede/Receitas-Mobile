abf7be09e756d7b0951a330ae190e5d7afc96fd4
luscamamede <eumesmolucassilva@gmail.com>
2022-12-01 08:08:51 -0300

    modified:   lib/components/main_drawer.dart 	new file:   lib/screens/settings_screen.dart 	modified:   lib/utils/app_routes.dart

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import '../utils/app_routes.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  const MainDrawer({Key? key}) : super(key: key);
 
-  Widget _createItem(IconData icon, String label) {
+  Widget _createItem(IconData icon, String label, Function() onTap) {
     return ListTile(
       leading: Icon(
         icon,
@@ -12,11 +13,12 @@ class MainDrawer extends StatelessWidget {
       title: Text(
         label,
         style: const TextStyle(
-            fontFamily: 'RobotoCondensed',
-            fontSize: 24,
-            fontWeight: FontWeight.bold),
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
       ),
-      onTap: (() {}),
+      onTap: onTap,
     );
   }
 
@@ -24,25 +26,34 @@ class MainDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
-        children: <Widget>[
+        children: [
           Container(
+            height: 120,
             width: double.infinity,
-            height: 110,
-            padding: const EdgeInsets.all(10),
-            color: Theme.of(context).colorScheme.primary,
-            alignment: Alignment.center,
-            child: const Text(
-              'Vamos Cozinhar!',
+            padding: const EdgeInsets.all(20),
+            color: Theme.of(context).colorScheme.secondary,
+            alignment: Alignment.bottomRight,
+            child: Text(
+              'Vamos Cozinhar?',
               style: TextStyle(
-                fontWeight: FontWeight.w500,
+                fontWeight: FontWeight.w900,
                 fontSize: 30,
-                color: Colors.black,
+                color: Theme.of(context).colorScheme.primary,
               ),
             ),
           ),
           const SizedBox(height: 20),
-          _createItem(Icons.restaurant, 'Refeições'),
-          _createItem(Icons.settings, 'Configurações'),
+          _createItem(
+            Icons.restaurant,
+            'Refeições',
+            () => Navigator.of(context).pushReplacementNamed(AppRoutes.home),
+          ),
+          _createItem(
+            Icons.settings,
+            'Configurações',
+            () =>
+                Navigator.of(context).pushReplacementNamed(AppRoutes.settings),
+          ),
         ],
       ),
     );
